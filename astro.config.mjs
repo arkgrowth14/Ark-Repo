43e6af6b8b4835b033f47e337c796561ccbb92b7
arkgrowth14 <arkgrowth14@gmail.com>
2026-03-11 21:57:07 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react'; // ADD THIS
 
 export default defineConfig({
   site: 'https://arkgrowth14.github.io',
   base: '/Ark-Repo',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(), 
+    react() // ADD THIS
+  ],
 });
